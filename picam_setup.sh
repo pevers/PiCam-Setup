a3cdc52248ae74bfd9c5601be6b3486837d491d6
Peter Evers <pevers90@gmail.com>
2013-01-30 16:39:18 +0100

    setup script edited

--- picam_setup.sh
+++ picam_setup.sh
@@ -2,11 +2,11 @@
 #  PiCam install script for initializing Raspberry Pi
 
 echo -e "====Installing dependencies====\n"
-apt-get -y build-essentials subversion libjpeg8-dev imagemagick
+apt-get install -y build-essential subversion libjpeg8-dev imagemagick
 echo -e "====Installing mpeg-streamer====\n"
 mkdir tmp
 cd tmp
 svn co https://mjpg-streamer.svn.sourceforge.net/svnroot/mjpg-streamer mjpg-streamer
-cd mjpeg-streamer
+cd mjpg-streamer
 make
 echo -e "====DONE====\n"
